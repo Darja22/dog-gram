1b7f6bdc72ea0b65164f1cf793ce21d18d0e2656
Darya <example@gmail.com>
2022-02-02 12:54:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,80 @@
+//  console.log(1);
+
+// let number = 5;
+// const LeftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a:50
+
+// };
+
+// obj.a=10;
+// console.log(obj);
+            // УРОК 6
+// let number = 4.8;
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+
+//  const bool = true;
+  
+//  console.log(something);
+
+//  const obj = {
+//      name: 'John',
+//      age: 25,
+//      isMarried: false,
+//      ready: true
+// };
+// console.log(obj.ready);
+
+// let arr = ['apple.png', 'orange.png', 'plump.png'];
+// console. log(arr[2]);
+
+// let arr = ['a', 's'];
+// console.log(arr[0]+ +arr[0]+arr[0]+arr[1]);
+// console.log('a'+ +'a'+'a'+'s');
+// console.log('b'+'a'+ + 'a'+'a');
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?','');
+// console.log(answer + 5); 
+// // сложения не произойдет, просто строка приклеит цифру, введенную пользователем
+// const answer =+prompt('Вам есть 18?','');
+// console.log(answer + 5); 
+// // сложение происходит, т.к + добавлен еще и на prompt.
+// const answers=[];
+
+// answers[0]=prompt('Имя','');
+// answers[1]=prompt('Фамилия','');
+// answers[2]=prompt('Возраст','');
+
+// console.log(typeof(answers));
+
+// const category='toys';
+// console.log(`https://someurl.com/ ${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Привет,  ${user}`);
+
+// const user = 'а';
+// const user3 = 's';
+
+// console.log(`b${user}${'a'*8}${user}${user3}`);
+
+ console.log(5%2);
+ console.log(2*4=='8');
+
+ console.log(2*4===8);
+ console.log(2*4==='8');
+
+ console.log(2+2*2==8);
